d257acc32421f18a8fbb932a2ca3b52c873de448
laurene88 <lauren.eddington@hotmail.com>
2024-05-03 12:51:53 +1200

    Add project files.

---
+++ Assets/Scripts/SceneButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButton : MonoBehaviour
+{
+    public void ChangeScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void ClickBackButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
